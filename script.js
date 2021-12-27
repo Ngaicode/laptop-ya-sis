20287e585cd24e6bc8447f602b25d13be1994247
Ngaicode <Mjombakevin376@gmail.com>
2021-12-27 21:12:40 +0300

    add one more log to the console

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function iAmBlessed() {
     console.log('I get to learn all these new exiting things that intrest me');
     console.log('I really should be more grateful for this');
     console.log('This is what im learning');
-
+    console.log('im so grateful i get to have all this great learning material')
 }
 
 // calling the function
