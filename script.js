5948f4fee59d80bad90d639fa34a110e5a03d48a
Ngaicode <Mjombakevin376@gmail.com>
2021-12-27 20:13:32 +0300

    log to the console

--- script.js
+++ script.js
@@ -3,6 +3,10 @@
 // function declaration
 function iAmBlessed() {
     console.log('i am blessed');
+    console.log('I get to learn all these new exiting things that intrest me');
+    console.log('I really should be more grateful for this');
+    console.log('This is what im learning');
+
 }
 
 // calling the function
